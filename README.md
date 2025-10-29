cb6537a37f681eb9ab64c9a1fb9db03754a2e924
Vincent Lo <vincent.lo@telecom-sudparis.eu>
2025-10-29 18:16:49 +0900

    gray scott implementation

---
+++ README.md
@@ -0,0 +1,23 @@
+# A Comparative Study of Diffusion Models and Long Short-Term Memory Networks throught the Reaction–Diffusion Equation
+
+This is a Research Project led in the Mathematical Informatics laboratory at Nara Institute of Science and Technology.
+
+## Abstract 
+Understanding how complex patterns emerge from simple rules is a central question in both biology and artificial intelligence. In biological systems, Alan Turing’s Reaction–diffusion (RD) model provides an explanation for spontaneous pattern formation. It describes how interacting chemical substances, an activator and an inhibitor, diffuse through space and react locally, generating stable spatial patterns such as stripes, spots, or waves. This interaction between local activation and global inhibition has since been recognized as a general mechanism for self-organization in living systems. In parallel, modern machine learning models have revealed similar dynamics in artificial systems. Long Short-Term Memory (LSTM) networks are a class of recurrent neural architectures designed to model temporal sequences and retain information over long time series data. They achieve this by regulating how past states influence future predictions, allowing them to capture both short-term and long-term dependencies in dynamic data. In a similar perspective, Denoising Diffusion Probabilistic Models (DDPMs) construct structured data from random noise through an iterative denoising process that progressively reverses a stochastic Gaussian noise diffusion. Although all these frameworks are used in different disciplines, they share a common mathematical foundation: the dynamic balance between growth and decay, reaction and diffusion. Investigating these parallels provides an understanding toward pattern formation, stability, and feedback control in both natural and artificial systems. However, the literature lacks a formal mathematical framework linking these two machine learning models. Existing studies rarely examine how the internal dynamics of artificial neural networks can be interpreted using concepts from biological reaction–diffusion system. This absence leaves unexplored a potentially rich analogy between biological self-organization and artificial memory regulation.
+
+In this work, we review the similarities between DDPMs and LSTM networks throught the Reaction-diffusion equation. Morphogen concentrations variate depending on a diffusion and a reaction component. LSTM networks cell-state iterative equation is analogous to this mechanism but in a discrete framework due to the its squashing functions. Likewise, DDPMs viewed as continuous-time stochastic differential equations highlights a striking analogy thought the presence of both drift and diffusion terms. Hence, a general mathematical equation can emerge that unifies the three models main output. The concept of short-range positive feedback and long-range negative feedback in the RD model emphasize the parallel as well. In LSTM networks, the input gate, which amplifies and stores relevant signals, and the forget gate, which dissipates or suppresses irrelevant information, respectively act as the activator and the inhibitor in the RD model. In DDPMs, the denoising network provides local positive feedback, restoring structure lost through noise, while the diffusion process applies global negative feedback, spreading and stabilizing information
+
+## Objectives
+
+In this git repository, we aim to reproduce turing patterns using a LSTM RNN and DDPM and then compare the result in stability and complexity. This first step is to implement the Reaction-Diffusion equation system.
+
+## Gray-Scott Model
+
+$
+\begin{align}
+    \frac{\partial U}{\partial t} &= D_U \nabla^2 U - UV^2 + F(1-U)\\
+    \frac{\partial V}{\partial t} &= D_V \nabla^2 V + UV^2 - (F+k) V
+\end{align}
+$
+
+Where the terms containing $D_U$ and $D_V$ are the diffusion terms and the rest are reaction terms.
--- main.py
+++ main.py
@@ -1,6 +1,124 @@
-def main():
-    print("Hello from lstm-ddpm!")
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import time
 
+def laplacian(u,h):
+    return (u[2:, 1:-1] + u[1:-1, 2:] + u[:-2, 1:-1] + u[1:-1, :-2] - 4 * u[1:-1, 1:-1]) / h**2
+
+def periodic_bound_conditions(u):
+    u[0, :] = u[-2, :]
+    u[-1, :] = u[1, :]
+    u[:, 0] = u[:, -2]
+    u[:, -1] = u[:, 1]
+
+class GrayScott:
+    def __init__(self, F=0.04, k=0.06, D_u=2e-5, D_v=1e-5, x0=-1, x1=1, N=256):
+        self.F = F
+        self.k = k
+        self.D_u = D_u
+        self.D_v = D_v
+        self.image_frame = 0
+
+        Nnodes = N + 1
+        
+        range = x1 - x0
+        dx = range / N
+        self.x0 = x0
+        self.x1 = x1
+        self.dx = dx
+        self.dt = dx**2 / (5*max(D_u, D_v))
+
+        self.x, self.y = np.meshgrid(np.linspace(x0-dx, x1+dx, Nnodes+2), np.linspace(x0-dx, x1+dx, Nnodes+2))
+
+        # Concentration definition
+        self.u = np.zeros((Nnodes+2, Nnodes+2))
+        self.v = np.zeros((Nnodes+2, Nnodes+2))
+        self.u[1:-1, 1:-1] = 1 - np.exp(-80*((self.x[1:-1, 1:-1]+0.05)**2 + (self.y[1:-1, 1:-1]+0.05)**2))
+        self.v[1:-1, 1:-1] = np.exp(-80*((self.x[1:-1, 1:-1]-0.05)**2 + (self.y[1:-1, 1:-1]-0.05)**2))
+
+        noise_strength = 0.05 # between 0.005 and 0.05
+        self.u[1:-1, 1:-1] += noise_strength * (np.random.rand(*self.u[1:-1, 1:-1].shape) - 0.5)
+        self.v[1:-1, 1:-1] += noise_strength * (np.random.rand(*self.v[1:-1, 1:-1].shape) - 0.5)
+
+        periodic_bound_conditions(self.u)
+        periodic_bound_conditions(self.v)
+    
+    def _dump(self, time, both=True):
+        x = self.x[1:-1, 1:-1]
+        y = self.y[1:-1, 1:-1]
+        U = self.u[1:-1, 1:-1]
+        V = self.v[1:-1, 1:-1]
+        if both:
+            fig, ax = plt.subplots(1, 2, figsize=(16, 8))
+            ax[0].contourf(x, y, U, levels=50, cmap='jet')
+            ax[1].contourf(x, y, V, levels=50, cmap='jet')
+            fig.suptitle(f"time = {time:e}")
+            lim = (self.x0, self.x1)
+            species = ("U", "V")
+            for a, l in zip(ax, species):
+                a.set_title(f"Species {l}")
+                a.set_xlabel("x")
+                a.set_ylabel("y")
+                a.set_xlim(lim)
+                a.set_ylim(lim)
+                a.set_aspect('equal')
+        else:
+            fig, ax = plt.subplots(1, 1, figsize=(6, 6))
+            ax.contourf(x, y, V, levels=50, cmap='jet')
+            fig.suptitle(f"time = {time:e}")
+            lim = (self.x0, self.x1)
+            ax.set_title(f"Species V")
+            ax.set_xlabel("x")
+            ax.set_ylabel("y")
+            ax.set_xlim(lim)
+            ax.set_ylim(lim)
+            ax.set_aspect('equal')
+        fig.savefig(os.path.join(".", f"frame_{self.image_frame:06d}.png"), dpi=400)
+        plt.close(fig)
+        self.image_frame += 1
+
+    def update(self):
+        u_view = self.u[1:-1, 1:-1]
+        v_view = self.v[1:-1, 1:-1]
+
+        u_v2 = u_view * v_view * v_view
+        self.u[1:-1, 1:-1] += self.dt * (self.D_u * laplacian(self.u, self.dx) - u_v2 + self.F * (1 - u_view))
+        self.v[1:-1, 1:-1] += self.dt * (self.D_v * laplacian(self.v, self.dx) + u_v2 - (self.F + self.k) * v_view)
+
+        periodic_bound_conditions(self.u)
+        periodic_bound_conditions(self.v)
+
+    def integrate(self, t0, t1):
+        t = t0
+        s = 0
+
+        self._dump(t)
+        while t < t1:
+            if s % 100 == 0:
+                print(f"step={s}; time={t:e}")
+            self.update()
+            t += self.dt
+            if (t1 - t) < self.dt:
+                self.dt = t1 - t
+            s += 1
+        self._dump(t)
 
 if __name__ == "__main__":
-    main()
+    
+    F = 0.04
+    k=0.06
+    D_u=2e-5
+    D_v=1e-5
+    x0=-1
+    x1=1
+    N=256
+    time_length = 1500
+    
+    grayscott = GrayScott(F=F, k=k, D_u=D_u, D_v=D_v, x0=x0, x1=x1, N=N)
+
+    t0 = time.perf_counter()
+    grayscott.integrate(0, time_length)
+    t1 = time.perf_counter()
+
+    print(f"Execution time for a {time_length} ms sequence : {t1-t0} seconds")
