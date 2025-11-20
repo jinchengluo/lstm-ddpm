f22fbfca0647338d37d297796b2c096513731398
Vincent Lo <vincent.lo@telecom-sudparis.eu>
2025-11-20 18:20:29 +0900

    lstm train

--- src/lstm.py
+++ src/lstm.py
@@ -12,15 +12,17 @@ class LSTMCell(nn.Module):
         self.W_i = torch.randn(hidden_size, input_size)
         self.W_C = torch.randn(hidden_size, input_size)
         self.W_o = torch.randn(hidden_size, input_size)
+        
         self.U_f = torch.randn(hidden_size, hidden_size)
         self.U_i = torch.randn(hidden_size, hidden_size)
         self.U_C = torch.randn(hidden_size, hidden_size)
         self.U_o = torch.randn(hidden_size, hidden_size)
 
-        self.b_f = torch.zeros((hidden_size, 1))
-        self.b_i = torch.zeros((hidden_size, 1))
-        self.b_C = torch.zeros((hidden_size, 1))
-        self.b_o = torch.zeros((hidden_size, 1))
+        self.b_f = torch.zeros((1, hidden_size))
+        self.b_i = torch.zeros((1, hidden_size))
+        self.b_C = torch.zeros((1, hidden_size))
+        self.b_o = torch.zeros((1, hidden_size))
+
 
     def sigmoid(self, z):
         return 1 / (1 + torch.exp(-z))
@@ -29,14 +31,17 @@ class LSTMCell(nn.Module):
         return torch.tanh(z)
 
     def forward(self, x, h_prev, C_prev):
-        f_t = self.sigmoid(torch.matmul(self.W_f, x) + torch.matmul(self.U_f, h_prev) + self.b_f) # forget gate
-        i_t = self.sigmoid(torch.matmul(self.W_i, x) + torch.matmul(self.U_i, h_prev) + self.b_i) # input gate
+        f_t = self.sigmoid(torch.matmul(x, self.W_f.t()) + torch.matmul(h_prev, self.U_f.t()) + self.b_f) 
+        
+        i_t = self.sigmoid(torch.matmul(x, self.W_i.t()) + torch.matmul(h_prev, self.U_i.t()) + self.b_i) 
+        
+        C_tilda_t = self.tanh(torch.matmul(x, self.W_C.t()) + torch.matmul(h_prev, self.U_C.t()) + self.b_C) 
+        
+        C_t = f_t * C_prev + i_t * C_tilda_t 
 
-        C_tilda_t = self.tanh(torch.matmul(self.W_C, x) + torch.matmul(self.U_C, h_prev) + self.b_C) # candidate cell state
-        C_t = f_t * C_prev + i_t * C_tilda_t # cell state update
-
-        o_t = self.sigmoid(torch.matmul(self.W_o, x) + torch.matmul(self.U_o, h_prev) + self.b_o) # output gate
-        h_t = o_t * self.tanh(C_t) # hidden state update
+        o_t = self.sigmoid(torch.matmul(x, self.W_o.t()) + torch.matmul(h_prev, self.U_o.t()) + self.b_o) 
+        
+        h_t = o_t * self.tanh(C_t)
 
         dynamics = {"input": i_t, "forget": f_t, "cell_update": C_tilda_t}
         return h_t, C_t, dynamics
@@ -45,14 +50,17 @@ class LSTMCell(nn.Module):
 class LSTM(nn.Module):
     def __init__(self, input_size=1, hidden_size=16):
         super().__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
         self.cell = LSTMCell(input_size=input_size, hidden_size=hidden_size)
-        self.predictor = nn.Linear(input_size, hidden_size)
+        self.predictor = nn.Linear(hidden_size, input_size)
 
     def forward(self, x):
         # x shape: [Batch, Sequence_Len, 1]
         batch_size, seq_len, _ = x.size()
-        h = torch.zeros(batch_size, 16).to(x.device)
-        c = torch.zeros(batch_size, 16).to(x.device)
+        # print(batch_size, seq_len)
+        h = torch.zeros(batch_size, self.hidden_size).to(x.device)
+        c = torch.zeros(batch_size, self.hidden_size).to(x.device)
         
         outputs = []
         history = {"input": [], "forget": []}
